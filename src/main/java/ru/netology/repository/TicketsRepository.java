66cf4e76ae82bead2dad7f308a09db5aee9bd339
Megalapka <forvsykoe@list.ru>
2022-07-10 23:17:19 +0300

    Created java classes Tickets and TicketsRepository

---
+++ src/main/java/ru/netology/repository/TicketsRepository.java
@@ -0,0 +1,45 @@
+package ru.netology.repository;
+
+import ru.netology.domain.Tickets;
+
+public class TicketsRepository {
+    private Tickets[] tickets = new Tickets[0];
+
+    public void save(Tickets ticket) {
+// if (findById(id) != null) EXCEPTIONS
+        Tickets[] tmp = new Tickets[tickets.length + 1];
+        for (int i = 0; i < tickets.length; i++) {
+            tmp[i] = tickets[i];
+        }
+        tmp[tmp.length - 1] = ticket;
+        tickets = tmp;
+    }
+
+    public Tickets[] getTickets() {
+        return  tickets;
+    }
+
+    public Tickets findById(int id) {
+        for (Tickets ticket : tickets) {
+            if (ticket.getId() == id) {
+                return ticket;
+            }
+        }
+        return null;
+    }
+
+    public void removeById(int id) {
+       // if (findById(id) == null) EXCEPTIONS
+        Tickets[] tmp = new Tickets[tickets.length - 1];
+        int copyToIndex = 0;
+        for (Tickets ticket : tickets) {
+            if (ticket.getId() != id) {
+                tmp[copyToIndex] = ticket;
+                copyToIndex++;
+            }
+        }
+        tickets = tmp;
+    }
+
+
+}
